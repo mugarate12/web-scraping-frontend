366043c33086f62257db91603fe5b2e351c52819
Mateus Cardoso <serjumano17@gmail.com>
2021-10-21 07:50:24 -0300

    refactor!: add env variable to production url

--- config/socketIO.ts
+++ config/socketIO.ts
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client"
 
-const url = 'http://localhost:3333'
+const url = process.env.NODE_ENV === 'development' ? 'http://localhost:3333' : String(process.env.PRODUCTION_BASE_URL)
 const socket = io(url)
 
 export default socket
--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
+const dotenv = require('dotenv')
+
+dotenv.config()
+
 module.exports = {
   reactStrictMode: true,
+  env: {
+    PRODUCTION_BASE_URL: process.env.PRODUCTION_BASE_URL
+  }
 }
