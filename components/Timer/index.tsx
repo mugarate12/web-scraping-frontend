a0846e20d6c7bb76e54f0050738be858d526b68e
Mateus Cardoso <serjumano17@gmail.com>
2021-11-23 10:39:26 -0300

    fix!: timer

--- components/Timer/index.tsx
+++ components/Timer/index.tsx
@@ -16,13 +16,18 @@ export default function Timer({
   const [ timer, setTimer ] = useState<string>('')
 
   function updateTimer() {
-    const actualTime = moment()
-    const moreTime = moment(time)
+    // console.log(time);
 
-    const duration = moment.duration(actualTime.diff(moreTime))
-    // console.log(`${duration.minutes}:${duration.seconds()}`);
-
-    setTimer(`${duration.minutes()}:${duration.seconds()}`)
+    if (time === 'Executando') {
+      setTimer(time)
+    } else {
+      const actualTime = moment()
+      const moreTime = moment(time)
+  
+      const duration = moment.duration(actualTime.diff(moreTime))
+  
+      setTimer(`${duration.minutes()}:${duration.seconds()}`)
+    }
   }
 
   useEffect(() => {
