cf0cb53247a1e017245fdf887b9d601b6277faf3
Mateus Cardoso <serjumano17@gmail.com>
2021-11-29 10:09:48 -0300

    fix!: connect server config

--- config/axios.ts
+++ config/axios.ts
@@ -4,8 +4,8 @@ const developmentURL = 'http://localhost:3333'
 const productionURL = String(process.env.PRODUCTION_BASE_URL)
 
 const api = axios.create({
-  // baseURL: process.env.NODE_ENV === 'development' ? developmentURL : productionURL
-  baseURL: 'http://3.86.91.4:3333'
+  baseURL: process.env.NODE_ENV === 'development' ? developmentURL : productionURL
+  // baseURL: 'http://3.86.91.4:3333'
 })
 
 export default api
